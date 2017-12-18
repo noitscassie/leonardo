428de8d1fe4083b1d3e43b1eb0840c72b3b8d2bd
Peter Johnstone <peter.wdj@gmail.com>
2017-12-18 06:46:23 +0000

    Add test for new post button to homepage spec

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
-feature 'Homepage is set up' do
+feature 'Homepage' do
   scenario 'User sees "Leonardo" on the homepage' do
     visit root_url
     expect(page).to have_content 'leonardo'
+  end
+
+  scenario 'Homepage should have link taking user to page to add new photo' do
+    visit root_url
+    expect(page).to have_link 'New Post'
   end
 end
