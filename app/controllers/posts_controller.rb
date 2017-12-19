850fbf8f06b8f8d7b92e2dc462457841557a94de
Peter Johnstone <peter.wdj@gmail.com>
2017-12-19 09:11:03 +0000

    Authenticate user before allowing them to create a enw post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   def new
   end
 
+  before_action :authenticate_user!
   def create
     @post = Post.create(image: params[:image], description: params[:description])
     redirect_to root_url
