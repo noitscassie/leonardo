5165a4817bd12a20b9a9f52463cfc95f7ebc0af6
Peter Johnstone <peter.wdj@gmail.com>
2017-12-19 06:38:26 +0000

    Add test to check for form allowing user to upload new post

--- spec/features/new_post_spec.rb
+++ spec/features/new_post_spec.rb
@@ -5,4 +5,10 @@ feature 'New post page' do
     visit '/posts/new'
     expect(page).to have_content 'Add new post'
   end
+  scenario 'Page has form allowing the user to upload a new post' do
+    visit '/posts/new'
+    expect(page).to have_field 'image'
+    expect(page).to have_field 'description'
+    expect(page).to have_button 'Save'
+  end
 end
