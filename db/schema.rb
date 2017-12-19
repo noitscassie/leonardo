84d07799d93c1fe373576a5027c473603346b5f7
Peter Johnstone <peter.wdj@gmail.com>
2017-12-19 06:32:29 +0000

    Updated schema including image and description properties for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171218063033) do
+ActiveRecord::Schema.define(version: 20171218103350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20171218063033) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.string "image"
   end
 
 end
