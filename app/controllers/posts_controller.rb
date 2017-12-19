9d44c566f3fa0fba74a3e6ef0f21776ae13042fb
Peter Johnstone <peter.wdj@gmail.com>
2017-12-19 06:33:47 +0000

    Create new post and redirect to homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,9 @@ class PostsController < ApplicationController
 
   def new
   end
-  
+
+  def create
+    @post = Post.create(image: params[:image], description: params[:description])
+    redirect_to root_url
+  end
 end
