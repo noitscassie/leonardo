83f40a40332f262bff492ab0841df2bd4bef5796
Peter Johnstone <peter.wdj@gmail.com>
2017-12-20 08:56:04 +0000

    Configure FactoryBot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'database_cleaner'
+require 'factory_bot_rails'
+require 'support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -55,6 +57,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # PJ: Configure ControllerMacros for FactoryBot
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, :type => :controller
+  config.include Warden::Test::Helpers
 
   # PJ: Configure database_cleaner
   config.before(:suite) do
