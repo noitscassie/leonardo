714252bb60ce48f9c184f0505ce72be272c895f5
Peter Johnstone <peter.wdj@gmail.com>
2017-12-21 08:48:26 +0000

    Add necessary properties to User for Facebook OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
